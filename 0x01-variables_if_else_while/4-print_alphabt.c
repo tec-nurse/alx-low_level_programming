3db411f407a5c737bb0530d2e2734f3673e395c0
tec-nurse <jakosile@gmail.com>
2022-09-12 12:06:19 -0700

    tasksa combine

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Description: prints lowercase alphabets
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch != 'e' && ch !='q')
+	{
+		if (ch != 'e' && ch !='q')
+		{
+			putchar(ch);
+		}
+		else
+		{
+		}
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Prints all single digits of base 16 followed by new line
+ *
+ * Return: returns 0
+ */
+int main(void)
+{
+	int digit;
+
+	for (digit = '0'; digit <= '9'; digit++)
+	{
+		digit = 'a';
+		for (; digit <= 'f'; digit++)
+		{
+			putchar(digit);
+		}
+		break;
+	}
+}
+putchar('\n');
+return (0);
+}
