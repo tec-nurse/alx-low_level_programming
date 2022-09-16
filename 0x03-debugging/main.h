dad95919ad1cf3f3e3187d02dce0f5428ec8da7b
tec-nurse <jakosile@gmail.com>
2022-09-15 22:50:11 -0700

    task0

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#DEFINE MAIN_H
+
+#incude <stdio.h>
+
+void positive_or_negative(int i);
+int largest_number(int, int, int);
+void print remaining_days(int, int, int);
+int convert_day(int, int);
+
+#endif /* MAIN_H */
