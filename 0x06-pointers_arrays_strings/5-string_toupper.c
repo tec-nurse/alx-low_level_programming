27ee649acb48fae2b4689d3b4ca8896f78bbe421
tec-nurse <jakosile@gmail.com>
2022-09-22 21:30:36 -0700

    tsk5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,7 +2,7 @@
 /**
  * string_toupper - changes all lowercase letters of a string to uppercase
  * @str: string
- * Return: returns char
+ * Return:str
  */
 
 char *string_toupper(char *str)
@@ -12,7 +12,7 @@ int i;
 for (i = 0; str[i] != '\0'; i++)
 {
 if (str[i] >= 'a' && str[i] <= 'z')
-}
+{
 str[i] = str[i] - 32;
 }
 }
